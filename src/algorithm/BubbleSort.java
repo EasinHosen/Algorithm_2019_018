5177d5817e5287e3273f70f03143b9d01167265b
EasinHosen <easinhosen5@gmail.com>
2023-01-22 10:35:33 +0600

    bubble sort modified output

--- src/algorithm/BubbleSort.java
+++ src/algorithm/BubbleSort.java
@@ -9,30 +9,37 @@ import java.util.Scanner;
 
 /**
  *
- * @author ASUS
+ * @author easin
  */
 public class BubbleSort {
+
     Scanner s = new Scanner(System.in);
-    
-    public void bubbleSort(){
-        
+
+    public void bubbleSort() {
+
         Input in = new Input();
         int list[] = in.listInput();
-        
+
         int n = list.length;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n-i-1; j++){
-                if(list[j] > list[j+1]){
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nPass " + i + ":");
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list[j] > list[j + 1]) {
                     int temp = list[j];
-                    list[j] = list[j+1];
-                    list[j+1] = temp;
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
                 }
+                for (int k = 0; k < n; k++) {
+                    System.out.print(list[k] + " ");
+                }
+                System.out.println("");
             }
+
         }
-        
+
         System.out.print("Sorted output: ");
-        for(int i=0; i<n; i++){
-            System.out.print(list[i]+" ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(list[i] + " ");
         }
     }
 }
--- src/algorithm/Input.java
+++ src/algorithm/Input.java
@@ -9,13 +9,31 @@ import java.util.Scanner;
 public class Input {
     Scanner s = new Scanner(System.in);
     
+    
+//    public int[] listInput(int... a){
+////        System.out.println("Enter the number of element: ");
+////        int n = s.nextInt();
+//        System.out.println("Enter the numbers: ");
+//        
+////        int list[] = new int[n];
+////        for(int i =0; i<a.length; i++){
+////            System.out.print("index{"+i+"}= ");
+////            a[i] = s.nextInt();
+////        }
+//        for(int x:a){
+//            x=s.nextInt();
+//        }
+//
+//        return a;
+//    }
+    
     public int[] listInput(){
         System.out.println("Enter the number of element: ");
         int n = s.nextInt();
         System.out.println("Enter the numbers: ");
         
         int list[] = new int[n];
-        for(int i =0; i<n; i++){
+        for(int i =0; i<list.length; i++){
             System.out.print("index{"+i+"}= ");
             list[i] = s.nextInt();
         }
