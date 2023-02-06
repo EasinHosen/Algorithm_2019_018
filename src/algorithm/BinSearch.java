d1d8bed18a6eb132f188eb32c528154ea2b19bc1
EasinHosen <easinhosen5@gmail.com>
2023-02-06 09:46:39 +0600

    linear search added

--- src/algorithm/BinSearch.java
+++ src/algorithm/BinSearch.java
@@ -3,6 +3,8 @@ package algorithm;
 public class BinSearch {
     public void binarySearch() {
         Input in = new Input();
+        System.out.println("\n---Binary Search selected---");
+        System.out.println("\n\tInput must be sorted ASC!\n");
 
         int list[] = in.listInput();
         // int list[] = {12, 11, 13, 5, 6};
@@ -23,9 +25,9 @@ public class BinSearch {
             else
                 l = m + 1;
         }
-        if (found) {
-            System.out.println("Key found at index: " + m);
-        } else
+        if (found)
+            System.out.println("Key(" + key + ")found at index: " + m);
+        else
             System.out.println("Key not found.");
 
     }
